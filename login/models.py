82827fbf83a2499ccb8fc8d51ea0f53a41aef8bc
Eduardo Hansel <eduardohansel10@gmail.com>
2025-08-27 23:15:46 -0300

    Implementação do modelo de cadastro.

--- login/models.py
+++ login/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Credencial(models.Model):
+
+    login = models.CharField(max_length=45, unique=True)
+    senha = models.CharField(max_length=45)
+
+    class Meta:
+        verbose_name = "Credencial"
+        verbose_name_plural = "Credenciais"
+        db_table = "credenciais"
